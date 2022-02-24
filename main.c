6bf9ab32f9fdcd080917b8652924e385c511f776
haya1007 <hayato.cha20021007@gamil.com>
2022-02-24 14:16:18 +0900

    テストコミット

--- main.c
+++ main.c
@@ -14,5 +14,6 @@ int main(int argc, char *argv[]){
 
         /*ここまでBotプログラムを書く*/
            sleep(0.1);
+           /* うんち */
     }
 }
---
+++ python/minecraft/test.py
@@ -0,0 +1,23 @@
+import sys
+sys.path.append('.')
+
+import init
+import initCameraPos
+import moveCameraRight
+import moveCameraLeft
+import moveCameraUp
+import moveCameraDown
+import clickLeft
+import clickRight
+import moveCharacterFowerd
+import moveCharacterBack
+import moveCharacterLeft
+import moveCharacterRight
+import setTime
+
+init.init()
+for i in range(36):
+    print(i) 
+    moveCameraLeft.moveCameraLeft()
+print("OK")
+init.init()
